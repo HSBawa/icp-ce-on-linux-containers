5892ab5081181b4fae35b565263abcf123e725ce
Harimohan S Bawa <HSBawa@users.noreply.github.com>
2018-02-05 17:27:42 -0500

    clean code.

--- running_process.sh
+++ running_process.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pod_check_interval=20
 running=$(lxc exec dev-master -- kubectl -s 127.0.0.1:8888 get pods --field-selector=status.phase=Running --no-headers=true --all-namespaces | wc -l)
-#running=45
 total=$(lxc exec dev-master -- kubectl -s 127.0.0.1:8888 get pods --no-headers=true --all-namespaces | wc -l)
 if [ "$running" != "$total" ]; then
     echo "Not all pods are running. Checking pod status every $pod_check_interval seconds."
@@ -10,6 +9,5 @@ while [ "$running" != "$total" ]; do
   echo -ne "$running/$total process are running"\\r
   sleep $pod_check_interval
   running=$(lxc exec dev-master -- kubectl -s 127.0.0.1:8888 get pods --field-selector=status.phase=Running --no-headers=true --all-namespaces | wc -l)
-  # running=$(($running+1))
 done
 echo "All $running/$total processes are running. ICP is now ready for use."
