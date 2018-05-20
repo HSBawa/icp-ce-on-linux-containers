590a6d820535cf91fad15cbab478856bc145b627
H S Bawa <>
2018-05-20 02:36:27 -0400

    Add icp  pv yaml

--- k8s/liberty/print-console-url.sh
+++ k8s/liberty/print-console-url.sh
@@ -5,6 +5,6 @@ echo "********************************************************"
 echo ""
 NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services my-liberty-ibm-open-libe-np)
 NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
-echo "Console URL : http://$NODE_IP:$NODE_PORT"
+echo "Liberty page URL : http://$NODE_IP:$NODE_PORT"
 echo "**Note: Change to Proxy IP, if the script retrieved IP does not work"
 echo ""
--- k8s/webterminal/print-console-url.sh
+++ k8s/webterminal/print-console-url.sh
@@ -3,9 +3,8 @@ echo "********************************************************"
 echo "This script assumes that ICP config context is set"
 echo "********************************************************"
 echo ""
-NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services my-cloudant-ibm-cloudant-dev)
+NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services web-terminal-ibm-webterm)
 NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
-echo "Cloudant admin console URL : http://$NODE_IP:$NODE_PORT/dashboard.html"
-echo ""
-echo "Console default username/password: admin/pass"
+echo "Web terminal URL : http://$NODE_IP:$NODE_PORT"
+echo "**Note: Change to Proxy IP, if the script retrieved IP does not work"
 echo ""
