7b4f35357385bce8195386d3328034c9479f6523
Harimohan S Bawa <HSBawa@users.noreply.github.com>
2018-08-14 20:56:15 -0400

    Script to install terraform and packer clis

---
+++ install-terra-n-packer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt-get update
+apt install -y curl unzip
+
+PACKER_VERSION=1.2.5
+TERRAFORM_VERSION=0.11.7
+
+PACKER_ZIP="packer_${PACKER_VERSION}_linux_amd64.zip"
+PACKER_URL="https://releases.hashicorp.com/packer/${PACKER_VERSION}/${PACKER_ZIP}"
+
+TERRAFORM_ZIP=terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/${TERRAFORM_ZIP}"
+
+echo ""
+echo "Installing packer ..."
+curl -fso /tmp/${PACKER_ZIP} ${PACKER_URL}
+unzip /tmp/${PACKER_ZIP} -d /usr/local/bin
+
+
+echo ""
+echo "Installing terraform ..."
+curl -fso /tmp/${TERRAFORM_ZIP} ${TERRAFORM_URL}
+unzip /tmp/${TERRAFORM_ZIP} -d /usr/local/bin
+
+echo "$(which terraform)"
+echo "$(which packer)"
