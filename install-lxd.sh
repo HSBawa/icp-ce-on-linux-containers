f4cc01f89981ac339503176bfcfb5ecb5760f39e
Harimohan S Bawa <HSBawa@users.noreply.github.com>
2018-08-15 00:08:35 -0400

    Update install-lxd.sh

--- install-lxd.sh
+++ install-lxd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get update
-apt -y -t xenial-backports install lxd
+apt-get -y -t xenial-backports install lxd
+apt-get install -y thin-provisioning-tools
 echo "LXD client version: $(lxc --version)"
 echo "LXD server version: $(lxd --version)"
