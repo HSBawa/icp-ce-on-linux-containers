5600f0861d7d16410f80b38bab9e7911d6c0b55e
Harimohan S Bawa <HSBawa@users.noreply.github.com>
2018-10-08 00:57:53 -0400

    Update start_stop_cluster.sh

--- start_stop_cluster.sh
+++ start_stop_cluster.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
-  
-
+ 
 input=$1
 env=$2
 
 function invalid_command(){
-    echo "Usage: icpservers <start|stop|restart> <env>"
+    echo "This script will to start, stop or restart Docker on cluster nodes."
+    echo "Usage: start_stop_cluster.sh <start|stop|restart> <env>"
     exit
 }
 
-function start_stop_server(){
-
-     local vms=($(lxc list ${env}- -c n --format=csv))
-
-     if [[ $input =~ ^(start|stop|restart)$ ]]; then
-        for vm  in ${vms[*]}
-        do
-           echo "Changing server $vm state to: $input"
-           lxc exec $vm  -- sh -c "systemctl restart docker"
-        done
-
-     else
-        invalid_command
-
-     fi
+function start_stop_docker(){
+    local vms=($(lxc list ${env}- -c n --format=csv))
+    if [[ $input =~ ^(start|stop|restart)$ ]]; then
+       for vm  in ${vms[*]}
+       do
+          echo "Changing server $vm Docker state to: $input"
+          lxc exec $vm  -- sh -c "systemctl restart docker"
+       done
+    else
+       invalid_command
+    fi
 }
 
 if [[ -z "$input" ]]; then
-   invalid_command
+    invalid_command
 fi
 
 if [[ -z "$env" ]]; then
-   invalid_command
+    invalid_command
 fi
 
-start_stop_server
+start_stop_docker
