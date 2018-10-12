19fdcd3528e889aac4e7ea16def2a71cb3214487
Harimohan S Bawa <HSBawa@users.noreply.github.com>
2018-10-12 14:15:12 -0400

    Update lxd-env-setup.sh

--- util-scripts/lxd-env-setup.sh
+++ util-scripts/lxd-env-setup.sh
@@ -1,23 +1,63 @@
 #!/bin/bash
-#### Run this script as sudo/root
-mkdir -p /media/lxcshare
-bash "./docker/install-docker.sh"
-bash "./lxd-setup/install-lxd-on-ubuntu-w-apt.sh"
-## Install Terraform and Packer. Make sure to update latest version number
-bash "./lxd-setup/terraform-packer/install-terra-n-packer.sh"
-## Install Terraform Plugin for LXD. Make sure to get latest
-bash "./lxd-setup/terraform-plugin/install-terraform-plugin-for-lxd.sh"
-## Use following for Bionic host only. Requires Internet Access
-bash "packer validate ./lxd-setup/images/xenial-packer-lxd-image-lvm-for-bionic-host"
-bash "packer build ./lxd-setup/images/xenial-packer-lxd-image-lvm-for-bionic-host"
-## Use following for Xenial host only
-# bash packer validate ./lxd-setup/images/xenial-packer-lxd-image-lvm"
-# bash packer validate ./lxd-setup/images/xenial-packer-lxd-image-lvm
 
-## LVM Thin Provisioning
-# Creates new storage of type LVM pool-name=default, network-bridge=lxdbr0, lxd-profile=default
-sudo cat ./lxd-setup/init-pre-seed/lxd-init-preseed-lvm-default.yaml | lxd init --preseed
-# Creates new storage of type LVM pool-name=demo, network-bridge=demobr0, lxd-profile=demo
-#sudo cat ./lxd-setup/init-pre-seed/lxd-init-preseed-lvm-demo.yaml | lxd init --preseed
-# Creates new storage of type LVM pool-name=icp, network-bridge=icpbr0, lxd-profile=icp
-#sudo cat ./lxd-setup/init-pre-seed/lxd-init-preseed-lvm-demo.yaml | lxd init --preseed
+linux="xenial"
+
+
+if [[ $version =~ ^("xenial","bionic") ]]
+   linux=$1
+   echo "User provided LXD base image option is: $linux"   
+else 
+   echo "Invalid parameters. Default LXD bse image is set to: $linux"
+fi
+
+echo "### NOTE: THIS SCRIPT IS WORK IN PROGRESSS"
+echo "### PLEASE REVIEW AND UPDATE FOR YOUR USE"
+echo "Install will start in 10 secs ... CTRL-C to cancel"
+sleep 10
+
+function create_lxcshare_folder(){
+  #### Run this script as sudo/root
+  mkdir -p /media/lxcshare
+}
+
+function install_docker(){
+  bash "./docker/install-docker.sh"
+}
+
+function install_lxd(){
+   bash "./lxd-setup/install-lxd-on-ubuntu-w-apt.sh"
+}
+
+function install_terraform_and_packer_and_plugin(){
+   ## Install Terraform and Packer. Make sure to update latest version number
+   bash "./lxd-setup/terraform-packer/install-terra-n-packer.sh"
+   ## Install Terraform Plugin for LXD. Make sure to get latest
+   bash "./lxd-setup/terraform-plugin/install-terraform-plugin-for-lxd.sh"   
+}
+
+function init_lxd(){
+  ## LVM Thin Provisioning
+  # Creates new storage of type LVM pool-name=default, network-bridge=lxdbr0, lxd-profile=default
+  sudo cat ./lxd-setup/init-pre-seed/lxd-init-preseed-lvm-default.yaml | lxd init --preseed
+  # Creates new storage of type LVM pool-name=demo, network-bridge=demobr0, lxd-profile=demo
+  #sudo cat ./lxd-setup/init-pre-seed/lxd-init-preseed-lvm-demo.yaml | lxd init --preseed
+  # Creates new storage of type LVM pool-name=icp, network-bridge=icpbr0, lxd-profile=icp
+  #sudo cat ./lxd-setup/init-pre-seed/lxd-init-preseed-lvm-demo.yaml | lxd init --preseed
+}
+
+function create_image_for_icp(){
+  if [[ "bionic" == "$1" ]]; then
+     bash "packer validate ./lxd-setup/images/xenial-packer-lxd-image-lvm-for-bionic-host"
+     bash "packer build ./lxd-setup/images/xenial-packer-lxd-image-lvm-for-bionic-host"
+  else
+     bash packer validate ./lxd-setup/images/xenial-packer-lxd-image-lvm"
+     bash packer validate ./lxd-setup/images/xenial-packer-lxd-image-lvm  
+  fi
+}
+
+create_lxcshare_folder
+install_lxd
+init_lxd
+install_docker
+install_terraform_and_packer_and_plugin
+create_image_for_icp
