69e67d0518718cf1810f5e1f98458c8ce30c4c19
Harimohan S Bawa <HSBawa@users.noreply.github.com>
2018-10-12 13:51:04 -0400

    Update install-lxd-on-ubuntu-w-apt.sh

--- util-scripts/lxd-setup/install-lxd-on-ubuntu-w-apt.sh
+++ util-scripts/lxd-setup/install-lxd-on-ubuntu-w-apt.sh
@@ -3,7 +3,8 @@ echo "This will install LXD on Ubuntu"
 echo "NOTE: This script uses APT which will install 3.0.1"
 echo "For higher version of LXD, use SNAP instead of APT"
 apt-get update
-apt-get -y -t xenial-backports install lxd
+#apt-get -y -t xenial-backports install lxd
+snap install lxd --channel=stable
 apt-get install -y thin-provisioning-tools
 echo "LXD client version: $(lxc --version)"
 echo "LXD server version: $(lxd --version)"
