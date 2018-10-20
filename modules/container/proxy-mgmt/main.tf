b627c948519609c2e56c4920829e46cb86af1c1d
Harimohan S Bawa <HSBawa@users.noreply.github.com>
2018-10-20 01:48:25 -0400

    Update main.tf

--- modules/container/proxy-mgmt/main.tf
+++ modules/container/proxy-mgmt/main.tf
@@ -4,9 +4,9 @@
 resource "lxd_container" "icp_ce_mgmt" {
     count="${var.management_node[var.node_count]}"
     name  = "${var.environment[var.name_short]}-${var.management_node[var.name]}-${count.index}"
-    remote    = "${var.lxd_image[var.remote]}"
-    image     = "${var.lxd_image[var.image]}"
-    ephemeral = "${var.lxd_image[var.ephemeral]}"
+    remote    = "${var.lxd[var.remote]}"
+    image     = "${var.lxd[var.image]}"
+    ephemeral = "${var.lxd[var.ephemeral]}"
     profiles  = ["${var.icp_ce_profile_name}", "${var.icp_ce_mgmt_profile_name[count.index]}"]
 }
 
@@ -16,8 +16,8 @@ resource "lxd_container" "icp_ce_mgmt" {
 resource "lxd_container" "icp_ce_proxy" {
     count = "${var.proxy_node[var.node_count]}"
     name  = "${var.environment[var.name_short]}-${var.proxy_node[var.name]}-${count.index}"
-    remote    = "${var.lxd_image[var.remote]}"
-    image     = "${var.lxd_image[var.image]}"
-    ephemeral = "${var.lxd_image[var.ephemeral]}"
+    remote    = "${var.lxd[var.remote]}"
+    image     = "${var.lxd[var.image]}"
+    ephemeral = "${var.lxd[var.ephemeral]}"
     profiles  = ["${var.icp_ce_profile_name}", "${var.icp_ce_proxy_profile_name[count.index]}"]
 }
