45061654b553214fbfe97efed820727bdc478e94
Harimohan S Bawa <HSBawa@users.noreply.github.com>
2019-02-20 18:36:27 -0500

    Update install-docker.sh

--- util-scripts/docker/install-docker.sh
+++ util-scripts/docker/install-docker.sh
@@ -1,8 +1,13 @@
-echo [Installing docker-ce]
+echo [Installing latest Docker CE]
+CURL_INSTALLED="$(which curl)"
+if [[ -z "${CURL_INSTALLED}" ]]; then
+  echo "curl missing. Install command: sudo apt-get install curl -y"
+  exit -1
+fi
 curl -o /tmp/get-docker.sh -fsSL get.docker.com &> /dev/null
 sh /tmp/get-docker.sh &> /dev/null
 usermod -aG docker root &> /dev/null
 usermod -aG docker ubuntu &> /dev/null
-echo "Done."
-echo ""
 echo "$(docker version)"
+echo ""
+echo "Done."
