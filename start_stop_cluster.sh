bc729c8fdc3f226f1e86fe9856afcabfb85a9d13
HB <hsbawa@@users.noreply.github.com>
2019-03-06 22:42:21 +0000

    Update refreshed code and unified script

--- start_stop_cluster.sh
+++ start_stop_cluster.sh
@@ -6,21 +6,32 @@
 #####################################################################
 
 input=$1
-env=$2
+
+function  read_properties() {
+  while IFS== read -r KEY VALUE
+  do
+      if [[ "${KEY:0:1}" =~ ^[A-Z]$ ]]; then
+        export "$KEY=$VALUE"
+      fi
+  done < ./install.properties
+}
+
+
 
 function invalid_command(){
-    echo "This script will to start, stop or restart Docker on cluster nodes."
-    echo "Usage: start_stop_cluster.sh <start|stop|restart> <env>"
+    echo "This script will start, stop or restart Docker and Kubelet on cluster nodes."
+    echo "Usage: start_stop_cluster.sh <start|stop|restart>"
     exit
 }
 
 function start_stop_cluster(){
-    local vms=($(lxc list ${env}- -c n --format=csv))
+    local vms=($(lxc list ${ICP_ENV_NAME_SHORT}- -c n --format=csv))
     if [[ $input =~ ^(start|stop|restart)$ ]]; then
        for vm  in ${vms[*]}
        do
           echo "Changing server $vm Docker state to: $input"
           lxc exec $vm  -- sh -c "systemctl $input docker"
+          echo "Changing server $vm Kubelet state to: $input"
           lxc exec $vm  -- sh -c "systemctl $input kubelet"
        done
     else
@@ -32,8 +43,5 @@ if [[ -z "$input" ]]; then
     invalid_command
 fi
 
-if [[ -z "$env" ]]; then
-    invalid_command
-fi
 
 start_stop_cluster
--- terra-clean.sh
+++ terra-clean.sh
@@ -3,6 +3,6 @@
 ###############################################################################
 echo "This program is going to destory all terraform states in next 10 secs. Press Ctrl-C to cancel now."
 sleep 10
-rm -rf ./.terraform
-rm ./terraform.tfstate
-rm ./terraform.tfstate.backup
+rm -rf .terraform
+rm terraform.tfstate
+rm terraform.tfstate.backup
