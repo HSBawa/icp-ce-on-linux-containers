856feb9cc7080d341fc69478b0ac6023d7af7b6a
HB <hsbawa@@users.noreply.github.com>
2019-03-20 22:57:28 -0400

    Update screenshots

--- icp-login-3.1.2-ce.sh
+++ icp-login-3.1.2-ce.sh
@@ -21,8 +21,8 @@ echo  ""
 echo "[If you have issues executing cloudctl command, clean up ~/.cloudctl and ~/.helm]"
 echo  ""
 cloudctl login -a https://10.50.50.101:8443 \
-               -u hsbawa \
-               -p Adm1n_6754 \
+               -u admin \
+               -p Admin_827438 \
                -c id-devicpcluster-account \
                -n default \
                --skip-ssl-validation
